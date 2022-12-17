f7859c07824daf207d98c705c28f3e278cf8b91b
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2022-12-17 01:04:47 -0600

    Friday night
    worked on the UI for the form. pretty satisfactory.

--- src/components/FormInput/formInput.jsx
+++ src/components/FormInput/formInput.jsx
@@ -2,10 +2,28 @@ import React, {useState} from "react";
 import "../FormInput/formInput.css";
 
 const FormInput = (props) => {
+
+    console.log(props);
+    /// the 
+    const {label, errorMessage, onChange, id, ...inputProps } = props; /// destructuring
+    
+    // console.log(label);
+    // console.log(inputProps);
+
     return (
         <div className="formInput">
-            <h1>hello</h1>
+            <label>{label}</label>
+            <input className="input" {...inputProps}/>
+            {/* <input className="input" name={props.name} placeholder={props.placeholder} pattern={props.pattern} required={props.required} type={props.type}  /> */}
 
+            {/* <input 
+                    className="input" {...inputProps} 
+                    onChange={onChange} 
+                    onBlur={handleFocus} 
+                    onFocus={() => inputProps.name === "conFirmPassWord" && setFocused(true)} 
+                    focused={focused.toString()} 
+                /> */}
+            <span className="span">{errorMessage}</span>    
         </div>
     );
 }
--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -6,16 +6,40 @@ import { Link } from "react-router-dom";
 
 const Form = () => {
     
-    const [values, setValues] = useState({
-        fullname: "",
-        email: "",
-        birthDay:"",
-        password:"",
-        confirmPassword:"",
-        sso:"",
-    })
+    // const [values, setValues] = useState({
+    //     fullname: "",
+    //     email: "",
+    //     birthDay:"",
+    //     password:"",
+    //     confirmPassword:"",
+    //     sso:"",
+    // })
 
-    console.log(values);
+    const inputs = [
+        {
+            id: 1,
+            name: "sso",
+            type: "text",
+            placeholder: "SSO",
+            errorMessage: "SSO number is incorrect",
+            label: "SSO",
+            pattern: '^[0-9]+$', /// only numbers
+            required: true,
+        },
+        
+        {
+            id: 2,
+            name: "fullname",
+            type: "text",
+            placeholder: "Full Name",
+            errorMessage: "Full name should be 3-16 characters and should not include any special characters!",
+            label: "Full Name",
+            pattern: '^[A-za-z0-9 ]{3,16}$', 
+            required: true,
+        }
+    ];
+
+    // console.log(...inputs)
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -23,13 +47,22 @@ const Form = () => {
     }
 
     return (
-        <div>
-            <Link to="/"><button>Back</button></Link>
-            <div className='app'>
-                <form className="form" onSubmit={handleSubmit}>
-                    <h1>Register</h1>
-                    <button className='formInputButton'>Submit</button>
-                </form>
+        <div className='app'>
+            <div>
+                <div><Link to="/"><button>Back</button></Link></div>
+                <div className='imgcontainer'></div>
+                <div>
+                    <form className="form" onSubmit={handleSubmit}>
+                        <h1 className='register'>Register</h1>
+                        {/* <FormInput {...inputs[0]} /> */}
+                        {/* <FormInput {...inputs[1]} /> */}
+                        {inputs.map(input => (
+                            <FormInput key={input.id} {...input} />
+                            
+                            ))}
+                        <button className='formInputButton'>Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
